c7d2b0c85e7ecbcc8a37d33389fedd3fc8548064
kotekaman <haris.de.morata@gmail.com>
2022-06-08 10:35:53 +0700

    update http responses

---
+++ httpResponses/httpResponses.py
@@ -0,0 +1,46 @@
+class CustomHttpResponses(object):
+    def __init__(self):
+        pass
+
+    def setup_responses(self,msg, is_error, response_code, request_type, data):
+        dict = {
+            "msg": msg,
+            "type": request_type,
+            "response_code": response_code,
+            "is_error": is_error,
+            "data": data,
+        }
+
+        return dict
+
+
+    def post_success_responses(self,msg, data=None):
+        is_error = False
+        response_code = 200
+        request_type = "POST"
+
+        return self.setup_responses(msg, is_error, response_code, request_type, data)
+
+
+    def delete_success_responses(self,msg, data=None):
+        is_error = False
+        response_code = 200
+        request_type = "DELETE"
+
+        return self.setup_responses(msg, is_error, response_code, request_type, data)
+
+
+    def get_success_responses(self,msg, data=None):
+        is_error = False
+        response_code = 200
+        request_type = "GET"
+
+        return self.setup_responses(msg, is_error, response_code, request_type, data)
+
+
+    def put_success_responses(self,msg, data=None):
+        is_error = False
+        response_code = 200
+        request_type = "PUT"
+
+        return self.setup_responses(msg, is_error, response_code, request_type, data)
--- main.py
+++ main.py
@@ -3,51 +3,13 @@ from typing import Union
 from services.ddlServices import ddlServices
 from fastapi import FastAPI, Path, Request, Body
 from models.models import Database, Table, Column
+from httpResponses.httpResponses import CustomHttpResponses
 
 app = FastAPI()
 ddlSvc = ddlServices.DDLServices()
+customHttpresp = CustomHttpResponses()
 
 
-def setup_responses(msg, is_error, response_code, request_type, data):
-    dict = {
-        "msg": msg,
-        "type": request_type,
-        "response_code": response_code,
-        "is_error": is_error,
-        "data": data,
-    }
-
-    return dict
-
-
-def post_success_responses(msg, data=None):
-    is_error = False
-    response_code = 200
-    request_type = "POST"
-
-    return setup_responses(msg, is_error, response_code, request_type, data)
-
-def delete_success_responses(msg, data=None):
-    is_error = False
-    response_code = 200
-    request_type = "DELETE"
-
-    return setup_responses(msg, is_error, response_code, request_type, data)
-
-
-def get_success_responses(msg, data=None):
-    is_error = False
-    response_code = 200
-    request_type = "GET"
-
-    return setup_responses(msg, is_error, response_code, request_type, data)
-
-def put_success_responses(msg, data=None):
-    is_error = False
-    response_code = 200
-    request_type = "PUT"
-
-    return setup_responses(msg, is_error, response_code, request_type, data)
 
 
 @app.post("/api/v1/databases")
@@ -56,7 +18,7 @@ async def create_database(database: Database):
 
     msg = "success create database {}".format(database.database_name)
 
-    return post_success_responses(msg)
+    return customHttpresp.post_success_responses(msg)
 
 
 @app.post("/api/v1/tables")
@@ -65,7 +27,8 @@ async def create_table_with_default_schema(table: Table):
 
     msg = "success create table {}.{}".format(table.database_name, table.table_name)
 
-    return post_success_responses(msg)
+    return customHttpresp.post_success_responses(msg)
+
 
 @app.delete("/api/v1/tables")
 async def delete_table(table: Table):
@@ -73,7 +36,7 @@ async def delete_table(table: Table):
 
     msg = "success  delete {}.{}".format(table.database_name, table.table_name)
 
-    return delete_success_responses(msg)
+    return customHttpresp.delete_success_responses(msg)
 
 
 @app.post("/api/v1/{database}/{table}")
@@ -86,27 +49,28 @@ async def get_datas_from_table_that_already_defined(
 
     msg = "success get data from {}.{}".format(database, table)
 
-    return post_success_responses(msg, data_responses)
+    return customHttpresp.post_success_responses(msg, data_responses)
 
 
 @app.post("/api/v1/table_custom")
 async def create_table_with_spesific_columns(payload: dict = Body(...)):
     ddlSvc.create_table_with_columns(payload)
-    return post_success_responses("success create table")
+    return customHttpresp.post_success_responses("success create table")
 
 
 @app.get("/api/v1/table_ddl_history")
 async def create_table_ddl_history():
     ddlSvc.create_ddl_history_table()
+    return customHttpresp.get_success_responses("success create table")
 
-    return get_success_responses("success create table")
 
 @app.post("/api/v1/datas")
 async def insert_data(payload: dict = Body(...)):
     ddlSvc.insert_data(payload)
-    return post_success_responses("success insert datas")
+    return customHttpresp.post_success_responses("success insert datas")
+
 
 @app.put("/api/v1/tables")
 async def alter_table(payload: dict = Body(...)):
     ddlSvc.alter_table(payload)
-    return put_success_responses("success do alter operation")
+    return customHttpresp.put_success_responses("success do alter operation")
