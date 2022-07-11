d1517d8f05a86272d633faad71b25c28d488ad35
kotekaman <kotekaman@kotekamans-MacBook-Pro.local>
2022-07-11 18:30:36 +0700

    update example

--- main.py
+++ main.py
@@ -112,14 +112,51 @@ async def create_table_ddl_history():
 
 @app.post("/api/v1/datas")
 async def insert_data(payload: dict = Body(...,example={
-		"tb_name":"uji",
-		"db_name":"testing",
-		"datas":{
-			"name":"Abdul",
-			"address":"H Taha",
-			"age":32
-		}
-	})):
+    'tb_name': 'v1',
+    'db_name': 'devel2',
+    'datas':{
+    "userid":0,
+            "session_id":1111 ,
+            "ip":"192.168.100.1",
+            "user_agent":"Chrome",
+            "browser" :"chrome",
+            "browser_version" :123,
+            "os":"linux",
+            "os_version":"amd64",
+            "device":"android",
+            "device_name" :"alpine",
+            "country":"indonesia",
+            "region":"asia",
+            "city":"Jakarta",
+            "latitude":123.45,
+            "longitude":111.56 ,
+            "isp" :"Telkomsel",
+            "internet_speed":3336,
+            "app":"app",
+            "app_version":1,
+            "app_id" :"app_id",
+            "object_id":1,
+            "object_type":1,
+            "object_url":"http://www.testing.com",
+            "content_length":"Nullable(String)",
+            "title" :"Nullable(String)",
+            "collection_id" :123,
+            "content_list_id":123,
+            "partner_id" :123,
+            "event_type":"watch",
+            "event_value" :123456,
+            "bitrate":123456,
+            "width":123456,
+            "height" :123456,
+            "utm_source" :"utm_source",
+            "utm_term" :"utm_term",
+            "utm_id":123,
+            "utm_medium" :"utm_medium",
+            "referrer" :"referrer",
+            "referrer_path":"referrer_path"
+
+}
+    })):
     data_inserted,message= ddlSvc.insert_data(payload)
 
     if data_inserted != True:
